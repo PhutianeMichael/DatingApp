bbfc8907cb3e46608bd65f36ced80992d1c2f476
PhutianeMichael <phutianemichael@gmail.com>
2022-02-25 17:47:14 +0200

    Initial commit

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -27,9 +27,7 @@ namespace API.Controllers
 
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id){
-
             return await _context.Users.FindAsync(id);
-
         }
 
         [HttpPost]
