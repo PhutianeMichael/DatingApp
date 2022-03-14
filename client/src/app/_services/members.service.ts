6793ee303e25e1cbb22206d1132c0d5e3e8c7724
PhutianeMichael <phutianemichael@gmail.com>
2022-03-15 01:24:53 +0200

    cache members and load a member from the cache

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,7 @@
 import { UserParams } from './../_models/userParams';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
@@ -15,12 +15,16 @@ export class MembersService {
 
   baseUrl = environment.apiUrl;
   members: Member[] = [];
-
+  memberCache = new Map();
 
   constructor(private http: HttpClient) { }
 
 
   getMembers(userParams: UserParams){
+    var response = this.memberCache.get(Object.values(userParams).join('-'));
+    if(response){
+      return of(response);
+    }
 
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
@@ -29,12 +33,22 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + "users", params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + "users", params)
+      .pipe(map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      }));
   }
 
  getMember(username:string){
-   const member = this.members.find(x => x.username === username);
-  if(member !== undefined) return of(member);
+   const member = [...this.memberCache.values()]
+    .reduce((arr, elem) => arr.concat(elem.result), [])
+    .find((member: Member) => member.username === username);
+
+    if(member){
+       return of(member);
+    }
+
    return this.http.get<Member>(this.baseUrl + "users/" + username);
   }
 
