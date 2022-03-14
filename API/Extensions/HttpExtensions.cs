c27e156e048dd46a4b44443ceb1a8bde4cb2cc79
PhutianeMichael <phutianemichael@gmail.com>
2022-03-14 16:45:17 +0200

    implementing pagination in the backend

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+using API.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPagenationHeader(this HttpResponse response, int currentPage,
+         int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHearder(currentPage, itemsPerPage, totalItems, totalPages);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
