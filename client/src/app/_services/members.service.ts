decd188c7f04e47846d2e3033b0f43cad8ed936c
PhutianeMichael <phutianemichael@gmail.com>
2022-03-15 10:48:10 +0200

    Implement likes UI

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -82,6 +82,14 @@ export class MembersService {
     )
   }
 
+  addLike(username:string){
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string){
+    return this.http.get<Partial<Member[]>>(this.baseUrl + 'likes?predicate=' + predicate);
+  }
+
   setMainPhoto(photiId:number){
     return this.http.put(this.baseUrl + "users/set-main-photo/" + photiId, {});
   }
